269a0bb4df8ab5bdfccb6fc1288823383c0e374a
Jiho Yang <jy1737@my.bristol.ac.uk>
2020-05-26 23:44:50 +0900

    first commit

---
+++ todoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todoApp
+//
+//  Created by Jiho Yang on 26.05.20.
+//  Copyright © 2020 Jiho Yang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
